5b0d7e64b65e55c11c3b509d49c86a4d7ddc5bd9
Gustavo Leal <gustavo.leal@gmail.com>
2025-06-17 00:15:22 +0100

    Configurada a aplicação para poder rodar no site render.com

--- gerador_azulejo_api.py
+++ gerador_azulejo_api.py
@@ -6,36 +6,30 @@ import io
 
 from config.parametros_api import ParametrosApi
 
-app = Flask(__name__)
-caminho_modelo = ''
-tamanho_ruido = 100
+def levantar_api(caminho_arquivo_modelo: str=None):
+    app = Flask(__name__)
+    caminho_modelo = 'resultado/20250609113406/model/gerador_azulejos.h5'
+    tamanho_ruido = 100
+    caminho_modelo = caminho_arquivo_modelo if caminho_arquivo_modelo is not None else caminho_modelo
 
+    def recuperar_imagem_modelo(caminho_arquivo_modelo: str, dimensao_ruido: int):
+        gerador = keras.saving.load_model(caminho_arquivo_modelo)
+        ruido = tf.random.normal([1, dimensao_ruido])
+        imagem_gerada = gerador(ruido)
+        imagem_gerada = imagem_gerada.numpy().astype("float32")
+        imagem_gerada = imagem_gerada + 127.5
+        imagem_gerada = imagem_gerada * 127.5
+        imagem_gerada = imagem_gerada.astype("uint8")
+        resultado, buffer = cv2.imencode('.png', imagem_gerada[0])
+        return io.BytesIO(buffer)
 
-def recuperar_imagem_modelo(caminho_arquivo_modelo: str, dimensao_ruido: int):
-    print('No recuperar_imagem: ' + caminho_arquivo_modelo)
-    gerador = keras.saving.load_model(caminho_arquivo_modelo)
-    ruido = tf.random.normal([1, dimensao_ruido])
-    imagem_gerada = gerador(ruido)
-    imagem_gerada = imagem_gerada.numpy().astype("float32")
-    imagem_gerada = imagem_gerada + 127.5
-    imagem_gerada = imagem_gerada * 127.5
-    imagem_gerada = imagem_gerada.astype("uint8")
-    resultado, buffer = cv2.imencode('.png', imagem_gerada[0])
-    return io.BytesIO(buffer)
+    @app.route('/', methods=['GET'])
+    def gerar_azulejo():
+        return send_file(recuperar_imagem_modelo(caminho_modelo, tamanho_ruido), mimetype='image/png')
 
-
-@app.route('/gerar-azulejo', methods=['GET'])
-def gerar_azulejo():
-    return send_file(recuperar_imagem_modelo(caminho_modelo, tamanho_ruido), mimetype='image/png')
-
-
-def levantar_api(caminho_arquivo_modelo: str):
-    global caminho_modelo
-    caminho_modelo = caminho_arquivo_modelo
-    app.run(debug=True)
+    return app
 
 
 if __name__ == '__main__':
     parametros_aplicacao = ParametrosApi().recuperar_parametros()
-    print('No main: ' + parametros_aplicacao.caminho_arquivo_modelo)
-    levantar_api(parametros_aplicacao.caminho_arquivo_modelo)
+    levantar_api(parametros_aplicacao.caminho_arquivo_modelo).run(debug=True)
